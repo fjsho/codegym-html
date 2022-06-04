edad1311f66adef61c59424eb5919134e985cd5d
fjsho <gorimura7@gmail.com>
2022-06-04 11:03:25 +0900

    [add]gulpでsassの記載内容を整理するためのパッケージ導入

--- kaguya/gulpfile.js
+++ kaguya/gulpfile.js
@@ -1,9 +1,31 @@
-const gulp = require('gulp');
-const gulpPostcss = require('gulp-postcss');
-const cssDeclarationSorter = require('css-declaration-sorter');
+var gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var assets = require('postcss-assets');
+var cssdeclsort = require('css-declaration-sorter'); //①「css-declaration-sorter」を読み込み
  
-gulp.task('default', () => {
+gulp.task('css', () => {
   return gulp.src('./assets/css/style.css')
     .pipe(gulpPostcss([cssDeclarationSorter({order: 'smacss'})]))
     .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/_page-header.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})])) //②「sass」の後に指定
+    .pipe(postcss([assets({
+      loadPaths: ['./images']
+    })]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./sass/'));
 });
